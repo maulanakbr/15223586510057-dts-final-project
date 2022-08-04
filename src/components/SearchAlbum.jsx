688abaecb5d7f4dbc5898dccf62b1c1d67329fef
Maulana Akbar Yudistika <yudistika.akbar@gmail.com>
2022-08-04 20:46:13 +0700

    fix error

--- src/components/SearchAlbum.jsx
+++ src/components/SearchAlbum.jsx
@@ -71,7 +71,7 @@ const SearchBar = () => {
           <div className="input-group relative flex flex-wrap items-stretch w-full mb-8 rounded">
             <input
               type="search"
-              className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               placeholder="Search"
               aria-label="Search"
               aria-describedby="button-addon2"
