b8b3c31b6426bc0fec96eb6790a94f86a3d3945b
Maulana Akbar Yudistika <yudistika.akbar@gmail.com>
2022-08-04 19:47:22 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import Typed from "react-typed";
 
 const Navbar = () => {
   return (
     <div>
       <div className="flex py-6 mx-6 my-3 justify-center">
-        <h1 className="text-xl text-black">
-          <Typed
-            strings={["GET SPOTIFY ALBUM"]}
-            startDelay={100}
-            typeSpeed={100}
-            loop
-          />
-        </h1>
+        <h1 className="text-xl text-black">GET SPOTIFY ALBUM</h1>
       </div>
     </div>
   );
